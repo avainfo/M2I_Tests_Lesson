4b19550c4245ea7241a8ce3d7bd397d1876ac885
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-22 12:28:47 +0200

    feat: add button to fetch a todo item and update TodosService with HTTP client

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TodosService} from './services/todos.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('M2iTodoApp');
+
+  constructor(public todoService: TodosService) {
+  }
+
+  async clickButton() {
+    this.todoService.getTodo(1);
+  }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,10 +1,19 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Todo} from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  private http = inject(HttpClient);
+  private httpClient = inject(HttpClient);
+  private url = 'https://jsonplaceholder.typicode.com/todos';
 
+  async getTodo(id: number) {
+    this.httpClient.get<Todo>(this.url + '/' + id.toString()).subscribe(value => {
+      console.log(value);
+      console.log(value.completed);
+    });
+  }
 }
+// Todo : Global Service demo <T>
